d0add8ccf8bb751b173eb9c83596002b31830636
Caroline Feijo <contatodevcarol@gmail.com>
2024-08-20 10:00:17 -0300

    adicionando router view

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import roteador from "./roteador";
 
-import '@fortawesome/fontawesome-free/css/all.css'
-
-createApp(App).mount('#app')
+createApp(App).use(roteador).mount("#app");
--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,16 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Tarefas from "../views/Tarefas.vue";
 
 const rotas: RouteRecordRaw[] = [
+  {
     path: "/",
-    name: "tarefas",
-    component: 
-]
-
+    name: "Tarefas",
+    component: Tarefas,
+  },
+];
 
 const roteador = createRouter({
-history:createWebHashHistory(),
-routes:rotas
-})
+  history: createWebHashHistory(),
+  routes: rotas,
+});
 
 export default roteador;
-
